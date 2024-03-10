3f0779d37cc231633f42fd95bc74a50c11b15bd6
Ferdy Hahan Pradana <ferdyhahan5@gmail.com>
2024-03-11 04:55:25 +0700

    update: add url redis for vercel

--- queue/transactionQueue.js
+++ queue/transactionQueue.js
@@ -1,8 +1,11 @@
 import Queue from "bull";
 import { addTransaction } from "../services/transactionService.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // Initialize the queue outside of any function
-const transactionQueue = new Queue("transactions");
+const transactionQueue = new Queue("transactions", process.env.KV_REDIS_VERCEL);
 
 // Define the process handler to handle incoming jobs
 transactionQueue.process(async (job) => {
