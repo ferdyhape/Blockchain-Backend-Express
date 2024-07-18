3d8569a0a3acc9e444b8592837aa4dcb76e7a133
Ferdy Hahan Pradana <ferdyhahan5@gmail.com>
2024-07-19 05:34:25 +0700

    update: add getBlock for testing

--- routes/web3Route.js
+++ routes/web3Route.js
@@ -3,16 +3,23 @@ import {
   deployTransactionContract,
   deployTransactionDetailContract,
   deployTokenContract,
+  getAllBlocks,
+  getBlockByNumber,
+  getBlockFromTransactionHash,
 } from "../controllers/web3Controller.js";
 
 import { validateTokenDeploySmartContract } from "../middleware/validateToken.js";
+import { validateTokenGeneral } from "../middleware/validateToken.js";
 
 const router = express.Router();
 
-router.use(validateTokenDeploySmartContract);
+router.use(validateTokenGeneral);
 
-router.get("/deploy/transaction", deployTransactionContract);
-router.get("/deploy/transaction-detail", deployTransactionDetailContract);
-router.get("/deploy/token", deployTokenContract);
+// router.get("/deploy/transaction", deployTransactionContract);
+// router.get("/deploy/transaction-detail", deployTransactionDetailContract);
+// router.get("/deploy/token", deployTokenContract);
+router.get("/blocks", getAllBlocks);
+router.get("/block/:blockNumber", getBlockByNumber);
+router.get("/block/transaction/:transactionHash", getBlockFromTransactionHash);
 
 export default router;
